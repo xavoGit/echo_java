91796895450e5abe881ddd954c7a09665ef4c4af
Ruiz <ricardo.ruiz@nokia.com>
2022-11-14 00:54:54 -0600

    Initial commit

---
+++ src/main/java/com/xavo/echo/Application.java
@@ -0,0 +1,24 @@
+package com.xavo.echo;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+  @RequestMapping("/")
+  public String home() throws UnknownHostException {
+    return "Hello Docker World: " + InetAddress.getLocalHost();
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+}
